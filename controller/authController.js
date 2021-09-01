a6d55fd84b0cd74194d7944a35f57e3343cff712
Adil_Zakarin <62048169+Adil-web@users.noreply.github.com>
2021-09-01 17:35:28 +0600

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -77,8 +77,8 @@ class authController {
                         dataXml = dataXml.sbapi.header.error
                         if (dataXml['id'] == '0') {
                             const token = generateAccessToken(username, password)
-                            console.log({token})
-                            res.json({ token })
+                            console.log(token)
+                            res.json(token)
                         }
                         return res.status(400).json({ message: 'Неверный логин или пароль' })
                     } catch (e) {
